221b3052fae9d41d828be32fb4f02b32d1dfeb9c
The WA <a4ron5@yandex.ru>
2018-07-03 18:03:34 +0500

    add app

--- src/features/app/app.js
+++ src/features/app/app.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Container } from '../../ui/atoms'
 import { Input } from '../input'
+import { Mapka } from '../map'
+import { List } from '../routes'
+
+const LeftSide = styled.div`
+`
+const RightSide = styled.div`
+`
 
 export const App = (props) => (
   <Container>
-    <Input/>
+    <LeftSide>
+      <Input/>
+      <List/>
+    </LeftSide>
+    <RightSide>
+      <Mapka/>
+    </RightSide>
   </Container>
 )
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './main'
 import registerServiceWorker from './registerServiceWorker';
+import { injectGlobal } from 'styled-components'
+
+injectGlobal`
+  ul, li {
+    padding: 0;
+    margin: 0;
+    list-style-type: none;
+  }
+`
 
 ReactDOM.render(<Main />, document.getElementById('root'));
 registerServiceWorker();
