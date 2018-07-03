3207d4c53832b8bcb2008befb1c99cf293a462c7
The WA <a4ron5@yandex.ru>
2018-07-03 17:56:03 +0500

    add store

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,8 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 
-const rootReducer = combineReducers({})
+import { routes } from '../features/input'
+
+const rootReducer = combineReducers({ routes })
 
 export const store = createStore(rootReducer, applyMiddleware(thunk, logger))
