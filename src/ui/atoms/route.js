2397990771f9175f55dc966ab4f48d7de0294f9c
The WA <a4ron5@yandex.ru>
2018-07-04 14:19:28 +0500

    ui(route): fix

--- src/ui/atoms/route.js
+++ src/ui/atoms/route.js
@@ -1,9 +1,26 @@
+import React from 'react'
 import styled from 'styled-components'
 
-export const RouteUI = styled.li`
+export const RouteRaw = styled.li`
+  display: flex;
+  align-items: center;
   padding: 5px;
   border: 1px black solid;
   &:not(:nth-child(1)) {
     border-top: none;
   }
 `
+
+const Button = styled.div`
+  margin-left: auto;
+  width: 20px;
+  height: 20px;
+  background-color: black;
+`
+
+export const RouteUI = ({ text, onClick }) => (
+  <RouteRaw>
+    {text}
+    <Button onClick={onClick}></Button>
+  </RouteRaw>
+)
