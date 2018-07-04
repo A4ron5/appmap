0d74edf9e9a10bf126c31b8b939164306f4bae2a
The WA <a4ron5@yandex.ru>
2018-07-04 14:20:06 +0500

    feat(routes): fix

--- src/features/routes/listofroutes.js
+++ src/features/routes/listofroutes.js
@@ -2,18 +2,26 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { RoutesUI } from '../../ui/atoms'
 import { RouteUI } from '../../ui/atoms'
+import { removeRoute } from '../input/action'
 
-const ListRaw = (props) => {
-  const list = props.routes.map(item => {
-    return <RouteUI key={item}>{item}</RouteUI>
-  })
-  return (
-    <RoutesUI>
-     {list}
-    </RoutesUI>
-  )
+class ListRaw extends React.Component {
+
+  handleClick = (item) => {
+    this.props.removeRoute(item)
+  }
+
+  render() {
+    const list = this.props.routes.map(item => {
+      return <RouteUI onClick={(e) => this.handleClick(item)} key={item} text={item}/> 
+    })
+    return (
+      <RoutesUI>
+        {list}
+      </RoutesUI>
+    )
+  }
 }
 
-const mapStateToProps = state => ({ routes: state.routes.routes})
+const mapStateToProps = state => ({ routes: state.input.routes })
 
-export const List = connect(mapStateToProps)(ListRaw);
+export const List = connect(mapStateToProps, { removeRoute })(ListRaw);
